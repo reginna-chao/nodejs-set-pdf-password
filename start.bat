062192d44b4d8bb5fd57f701769fbd75999dc123
Reginna Chao <reginna.chao@ecloudvalley.com>
2024-03-05 10:31:31 +0800

    [feat] Add executable file (.bat for Windows, .sh for Linux and MacOS)

---
+++ start.bat
@@ -0,0 +1,5 @@
+rem Double-click this file, and it will automatically run 'yarn start'.
+@echo off
+cd /d "%~dp0"
+yarn start
+pause
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+yarn start
+read -p "Press Enter to continue..."
